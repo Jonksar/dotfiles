f34900406a8d5dac22db6ab917e548b864d50da0
Joonatan Samuel <joonatansamuel@Joonatans-MBP-2.home>
2022-06-28 15:56:37 +0300

    update install and add powerline install script

--- install.sh
+++ install.sh
@@ -1,3 +1,36 @@
+if [ "$(uname)" == "Darwin" ]; then
+    #running on Mac OS
+    echo "Running install on macos..."
+    echo "Install homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo "Install essentials..."
+    brew install zsh git
+
+    echo "Install oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    echo "Installing desktop apps..."
+    brew install --cask rectangle
+    brew install --cask whatsapp
+    brew install --cask skype
+    brew install --cask slack
+    brew install --cask google-chrome
+    brew install --cask warp
+
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    #running on Windows 
+    echo "Stop right now and reconsider your actions"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    #running on Linux
+    echo "Install essentials..."
+    sudo apt install git
+    sudo apt install zsh
+
+    echo "Install oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 mkdir $HOME/.vim
 
 DOTFILES_DIR=`echo $HOME`/.dotfiles
@@ -13,3 +46,11 @@ ln -sfv "$DOTFILES_DIR/.tmuxline" ~
 mkdir -p ~/.vim/templates
 ln -sfv "$DOTFILES_DIR/.vim/templates/c_template.txt" ~/.vim/templates
 ln -sfv "$DOTFILES_DIR/.vim/templates/py_template.txt" ~/.vim/templates
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install all vim plugins 
+vim +PluginInstall +qall
+
+# Tmux plugin manager install
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
